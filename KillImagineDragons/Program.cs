251819d01043c295ecf4e7a6768d42586ebec259
Ripal Patel <ripal212007@gmail.com>
2019-03-12 22:13:18 -0500

    Done

--- KillImagineDragons/Program.cs
+++ KillImagineDragons/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KillImagineDragons
 {
@@ -6,7 +8,30 @@ namespace KillImagineDragons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var goodSongs = new List<Song>();
+            var allSongs = new List<Song>();
+
+            allSongs.Add(new Song("Imagine Dragon", "Bleeding Out"));
+            allSongs.Add(new Song("Imagine Dragon", "Whatever It Takes"));
+            allSongs.Add(new Song("Imagine Dragon", "Friction"));
+            allSongs.Add(new Song("Imagine Dragon", "Amsterdam"));
+            allSongs.Add(new Song("Imagine Dragon", "Believer"));
+            allSongs.Add(new Song("Imagine Dragon", "Walking The Wire"));
+            allSongs.Add(new Song("Imagine Dragon", "It’s Time"));
+            allSongs.Add(new Song("Ed Sheeran", "Shape of You"));
+            allSongs.Add(new Song("Ray Charles ", "Georgia On My Mind "));
+            allSongs.Add(new Song("Roy Orbison", "Oh Pretty Woman "));
+            allSongs.Add(new Song("Dolly Parton", "I Will Always Love You"));
+            allSongs.Add(new Song("The Turtles", " Happy Together "));
+
+            goodSongs = allSongs.Where(song => song.Artist != "Imagine Dragon").ToList();
+
+            foreach (var song in goodSongs)
+            {
+                Console.WriteLine($"{song.Artist} : {song.Name}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ KillImagineDragons/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KillImagineDragons
+{
+    class Song
+    {
+        //properties
+        public string Artist { get; set; }
+        public string Name { get; set; }
+
+        //Constructors
+        public Song(string artist, string name)
+        {
+            Artist = artist;
+            Name = name;
+        }
+    }
+}
